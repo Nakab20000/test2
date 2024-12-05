74994a4eed0d934d8ae174217131460d3102c157
John Doe <johndoe@example.com>
2024-12-05 19:49:00 +0700

    for work

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class subject(models.Model):
+    Course_id = models.CharField(max_length=110,primary_key=True)
+    Course_name = models.CharField(max_length=100)
+    Teacher_name = models.CharField(max_length=100)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView
+from .models import subject
+from django.urls import reverse_lazy
+class Showdata(ListView):
+    model = subject
+    template_name = "Show.html"
+    context_object_name = "subject"
+    fields = ["Course_id","Course_name","Teacher_name"]
 
-# Create your views here.
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        query = self.request.GET.get("search",'')
+
+        if query:
+            queryset = queryset.filter(Course_name__icontains=query)
+        return queryset
+
+class Edit(UpdateView):
+    model = subject
+    template_name = "update.html"
+    fields = ["Course_name","Teacher_name"]
+    success_url = reverse_lazy("Show")
+
+class Del(DeleteView):
+    model = subject
+    template_name = "del.html"
+    fields = ["Course_name","Teacher_name"]
+    success_url = reverse_lazy("Show")
